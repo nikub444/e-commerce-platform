1abdfb7f03f56111fe01ce2f7053c3323f866025
nikub444 <magaszak12@gmail.com>
2020-04-30 18:20:10 +0200

    created project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>hehe</h1>
+                    <h2 className='subtitle'>XD</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
